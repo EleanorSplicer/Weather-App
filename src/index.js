16ea8ff2081e9258d6e0467952493b663dcee8eb
Kimberly D <134107722+EleanorSplicer@users.noreply.github.com>
2023-07-23 14:18:32 -0700

    Added Conversions

--- src/index.js
+++ src/index.js
@@ -31,21 +31,31 @@ function citySearch(event) {
 let form = document.querySelector("form");
 form.addEventListener("submit", citySearch);
 
-function changeFahrenheit(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = 98;
-}
-function changeCelcius(event) {
-  event.preventDefault();
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = 25;
-}
-let fahrenheitLink = document.querySelector("#f-link");
-fahrenheitLink.addEventListener("click", changeFahrenheit);
+  let temperatureElement = document.querySelector("#temperature");
 
-let celciusLink = document.querySelector("#c-link");
-celciusLink.addEventListener("click", changeCelcius);
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 function showWeather(response) {
   let cityElement = document.querySelector("#city");
